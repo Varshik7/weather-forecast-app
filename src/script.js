1478f49723808732c936cb6cd4f467eaf9d3ede8
Varshik7 <reddyvarshik9@gmail.com>
2025-10-06 11:15:39 +0530

    Implement 5-day forecast section

--- src/script.js
+++ src/script.js
@@ -1,4 +1,4 @@
-// Commit 4: Integrated OpenWeatherMap API
+// Commit 5:Add search and location functionality
 const apiKey = "4001eb904fe8618e2c32a814c977d189"; // Replace with your OpenWeatherMap API key
 const cityInput = document.getElementById("cityInput");
 const searchBtn = document.getElementById("searchBtn");
